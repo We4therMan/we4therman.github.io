6a90d0ec2020a93f177c74610399e4edd497affd
Yair Morales Franco <dr.m.twitch@gmail.com>
2020-01-14 13:28:36 -0800

    Updated GD site and literally did not change the .js at all.

--- app.js
+++ app.js
@@ -1,3 +1,11 @@
-console.log("This is my first JavaScript string!");
+console.log("This is my first JavaScript string!")
 //I will now math it up.
-console.log(5);
+console.log(5)
+
+let testThing = 'Hello'
+
+if (testThing == 'Hello') {
+  console.log('HOw you doin')
+} else {
+  console.log('HO')
+}
