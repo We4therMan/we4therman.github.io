c2fba3cc1b2c97ef3fae55cc62630d055c5c7e9c
We4therMan <dr.m.twitch@gmail.com>
2020-01-21 16:46:42 -0800

    added "broken rn"

--- app.js
+++ app.js
@@ -5,7 +5,8 @@ $(function() {
 
 function darkMode() {
   $(".background").css({"background": "none", "background-color": "black"})
-  $(".box").css({"background-color": "rgba(255,25,0,0.25)"})
-  $(".darkModeToggle span").text("Normal Mode")
+  $(".box").css({"background-color": "rgba(255,25,0,0.10)"})
+  $(".blueBox").css({"background-color": "rgba(0,0,255,0.1)"})
+  $(".darkModeToggle span").text("Normal Mode (broken rn)")
   $("this .onclick").function()
 }
