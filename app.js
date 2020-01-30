62fc5c18050e1f1f0b67158de3d465b4cc49a0db
We4therMan <dr.m.twitch@gmail.com>
2020-01-30 12:21:06 -0800

    cookie jar

--- app.js
+++ app.js
@@ -1,6 +1,7 @@
 $(function() {
   $("#darkMode").click(function(){
     darkMode = !darkMode
+    setCookie("darkMode", darkMode)
     refreshColors()
     count++
     console.log(count)
@@ -9,7 +10,7 @@ $(function() {
 
 })
 
-var darkMode = false
+var darkMode = (getCookie("darkMode") == "true") ? true : false
 
 var count = 0
 
@@ -53,4 +54,27 @@ function easterEgg() {
     $("#easterEgg").html("haha rekt")
     $("#darkMode").hide()
   }
+}
+
+function setCookie(cname, cvalue) {
+  var d = new Date();
+  d.setTime(d.getTime() + (999*24*60*60*1000));
+  var expires = "expires="+ d.toUTCString();
+  document.cookie = cname + "=" + cvalue + ";" + expires + ";path=/";
+}
+
+function getCookie(cname) {
+  var name = cname + "=";
+  var decodedCookie = decodeURIComponent(document.cookie);
+  var ca = decodedCookie.split(';');
+  for(var i = 0; i <ca.length; i++) {
+    var c = ca[i];
+    while (c.charAt(0) == ' ') {
+      c = c.substring(1);
+    }
+    if (c.indexOf(name) == 0) {
+      return c.substring(name.length, c.length);
+    }
+  }
+  return "";
 }
