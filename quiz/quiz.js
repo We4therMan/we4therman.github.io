49e113cdf78ec0bd405f0afb38899a47cae42b0d
We4therMan <dr.m.twitch@gmail.com>
2020-02-06 12:46:52 -0800

    first question maded

--- quiz/quiz.js
+++ quiz/quiz.js
@@ -3,5 +3,6 @@ $(function() {
 })
 
 
+
 console.log("Do NOT read the answers if you know how to read code. I don't like cheaters...")
 //The quiz! Do NOT read the answers if you know how to read code. I don't like cheaters...
