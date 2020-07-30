1a722d037d8ffbfd5f8f31bbed4e315e19942ae9
We4therMan <59382386+We4therMan@users.noreply.github.com>
2020-07-29 18:55:17 -0700

    added demonlist.js warning

--- gdlevels/demonlist.js
+++ gdlevels/demonlist.js
@@ -18,3 +18,7 @@ console.log("Secret 2: I dream of being a GD Mod.")
 console.log("Hey, you're not doing too bad! Keep looking for more secrets! You'll keep finding interesting stuff.")
 
 var demonAPI = "https://pointercrate.com/api/v1";
+
+
+//THIS APPLICATION IS VERY BROKEN AND INCOMPLETE. DO NOT UNDER ANY CIRCUMSTANCES TRY TO LOOK
+//AT THIS FOR YOUR OWN EDUCATIONAL PURPOSES OR YOU'LL LEARN HOW TO CODE WRONG THANKS.
